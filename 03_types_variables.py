ce5dd59327eaed0a898c3277b858db3f7b5fdee4
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-08-06 12:09:30 +0600

    added types.variables.py file

---
+++ 03_types_variables.py
@@ -0,0 +1,39 @@
+1. # Start with a Letter or Underscore
+# A variable name should start with a letter, like a, b, c, or an underscore _. It cannot start with a number.
+
+# Examples:
+apple = 5
+_apple = 6
+1apple = 10 #invalid due to number in the beginning
+
+
+2. # No Spaces Allowed
+# You cannot use spaces in variable names. Use an underscore _ instead.
+
+# Example:
+my_cat = "Meeka" #valid
+my cat = "Meeka" #Invalid due to space in between
+
+
+3. # Case Matters
+# small and Small are different in Python. So, be careful with big and small letters!
+
+# Example:
+apple = 5
+Apple = 10
+
+
+4. # Don't Use Python Keywords
+# Python has special words called keywords. We cannot use them as variable names.
+
+# Examples:
+and, for, while, if, True, False
+
+
+5. # No Special Symbols Like '@'
+# You cannot use special symbols like @ in variable names. Only letters, numbers, and underscores are allowed.
+
+# Example:
+@apple = 5 #invalid due to @ symbol
+app@le = 6 #invalid due to @ symbol
+
