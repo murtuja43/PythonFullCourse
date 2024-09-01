ae66292be899b4f6c275b9654b6083091be75433
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-09-01 07:07:42 +0600

    06_input.py

---
+++ 06_input.py
@@ -0,0 +1,14 @@
+a = int(input("What's A: "))
+b = int(input("What's B: "))
+
+print("a is:", a)
+print("b is:", b)
+
+print("sum of a and b is:", a + b)
+
+# name = input("What's your name?")
+# print("Welcome", name)
+
+
+name = input("What's your name?")
+print(f"Welcome {name} {a} {b}")
