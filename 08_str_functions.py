e7b3da17db146e6b256bbc0bea94fcfca35d8851
Md. Golam Murtuja Kayes <165343080+murtuja43@users.noreply.github.com>
2024-09-03 08:01:22 +0600

    added str functions file

---
+++ 08_str_functions.py
@@ -0,0 +1,10 @@
+name = "Trump is a human human"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.replace("human", "man"))
+print(name.find("p"))
+print(name.startswith("Tr"))
+print(name.endswith("man"))
+print(name.title())
